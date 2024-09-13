d3ebfb13b77fdf562dc850561c734ad54bf00fe7
erik suardiana <eriksuardiana644@gmail.com>
2024-09-13 10:48:32 +0800

    first commit

---
+++ src/app/page.jsx
@@ -0,0 +1,44 @@
+'use client'
+import { Html5QrcodeScanner, Html5QrcodeScanType } from 'html5-qrcode'
+import React, { useEffect, useState } from 'react'
+
+export default function Page() {
+
+    const [scanResult, setScanResult] = useState(null)
+
+    useEffect(()=>{
+        const scanner =  new Html5QrcodeScanner('reader', {
+            qrbox:{
+                width: 250,
+                height: 250
+            }, supportedScanTypes: [
+                // Html5QrcodeScanType.SCAN_TYPE_FILE, 
+                Html5QrcodeScanType.SCAN_TYPE_CAMERA
+            ],
+            fps: 5,
+            // aspectRatio: 1.7777778
+        })
+    
+        scanner.render(success, error);
+    
+        function success(result){
+            scanner.clear();
+            setScanResult(result)
+        }
+    
+        function error(err){
+            console.warn(err)
+        }
+    })
+ 
+  return (
+    <div className='w-full h-screen'>
+        {scanResult ? <div className="">Success <a href={scanResult}>{scanResult}</a></div>
+        :
+<div id='reader' className=""></div>
+
+        }
+        
+    </div>
+  )
+}
